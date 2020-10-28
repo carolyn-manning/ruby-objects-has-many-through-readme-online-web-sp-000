8d0f34de309d4bf982bfb63284b27621acb50be1
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-28 03:16:41 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,5 +13,15 @@ class Customer
   def self.all
     @@all
   end
- 
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+  Meal.all.select do |meal|
+    meal.customer == self
+  end
+end
+
 end
